062f7c5e93157b00f9e2c9131278c785c6193830
Victor Williams <victor.williams1030@gmail.com>
2020-05-15 22:00:03 +0000

    Automatically backed up by Learn

--- lib/min.rb
+++ lib/min.rb
@@ -1,10 +1,10 @@
 require 'pry'
 
 def get_the_min(groceries)
-groceries = {
-  dairy: ["milk", "yogurt", "cheese"],
-    vegetable: ["carrots", "broccoli", "cucumbers"],
-  meat: ["chicken", "steak", "salmon"],
-  grains: ["rice", "pasta"]
- }
+  groceries = {
+    dairy: ["milk", "yogurt", "cheese"],
+      vegetable: ["carrots", "broccoli", "cucumbers"],
+    meat: ["chicken", "steak", "salmon"],
+    grains: ["rice", "pasta"]
+   }
 end
